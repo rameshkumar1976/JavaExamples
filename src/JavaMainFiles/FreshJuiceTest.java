3a9d3b5bab7e5132bfd16da98dd64063467d6643
rameshkumar1976 <rameshkumar.radhakrishnan@gmail.com>
2017-04-19 13:42:06 +0200

    Updated Enum

---
+++ src/JavaMainFiles/FreshJuiceTest.java
@@ -0,0 +1,18 @@
+package JavaMainFiles;
+
+class FreshJuice {
+	enum FreshJuiceSize{SMALL,MEDIUM,LARGE}
+	FreshJuiceSize size;
+}
+
+
+public class FreshJuiceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FreshJuice juice = new FreshJuice();
+		juice.size = FreshJuice.FreshJuiceSize.MEDIUM ;
+	    System.out.println("Size: " + juice.size);
+	}
+
+}
---
+++ src/JavaMainFiles/Sample1_HelloWorld.java
@@ -0,0 +1,13 @@
+package JavaMainFiles;
+
+public class Sample1_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello ");
+		System.out.print("Java ");
+		System.out.print("World");
+	}
+
+
+}
